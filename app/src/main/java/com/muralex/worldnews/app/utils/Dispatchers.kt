914eb6c44658b5c8418cd7a3017850e80955e269
Alex-S <miralex.ca@gmail.com>
2022-09-04 16:49:20 -0400

    Dispatchers added

---
+++ app/src/main/java/com/muralex/worldnews/app/utils/Dispatchers.kt
@@ -0,0 +1,42 @@
+package com.muralex.worldnews.app.utils
+
+import kotlinx.coroutines.*
+
+interface Dispatchers {
+
+    fun launchUI(scope: CoroutineScope, block: suspend CoroutineScope.() -> Unit): Job
+
+    fun launchBackground(scope: CoroutineScope, block: suspend CoroutineScope.() -> Unit): Job
+
+    suspend fun changeToUI(block: suspend CoroutineScope. () -> Unit)
+
+    suspend fun <T> background(block: suspend CoroutineScope.() -> T): T
+
+    abstract class Abstract(
+        private val ui: CoroutineDispatcher,
+        private val background: CoroutineDispatcher,
+    ) : Dispatchers {
+
+        override fun launchUI(
+            scope: CoroutineScope,
+            block: suspend CoroutineScope.() -> Unit
+        ): Job = scope.launch(ui, block = block)
+
+        override fun launchBackground(
+            scope: CoroutineScope,
+            block: suspend CoroutineScope.() -> Unit
+        ): Job = scope.launch(background, block = block)
+
+        override suspend fun changeToUI(block: suspend CoroutineScope. () -> Unit) =
+            withContext(ui, block)
+
+        override suspend  fun <T> background(block: suspend CoroutineScope. () -> T) : T =
+            withContext(background, block)
+
+    }
+
+    class Base : Abstract(kotlinx.coroutines.Dispatchers.Main, kotlinx.coroutines.Dispatchers.IO)
+
+
+
+}
